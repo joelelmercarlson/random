b6532153e35cf10cfc91c1c50173e9dd9b7d8612
Joel E Carlson <joel_carlson@optum.com>
2020-08-25 16:53:28 -0500

    party_of_four

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,5 @@
 module Main where
   import System.Environment
-
-  import Character
   import DiceSet
   import Display (rpg)
   import Dwarf
@@ -14,17 +12,13 @@ module Main where
   main :: IO ()
   main = do
     xs <- getArgs
-
-    -- | dice pool
-    let x = zModRoll ["100", "d10"]
-
-    r <- roll x
+    r <- roll rollDice
 
     case nth 1 xs of
-      Just "dwarf"        -> rpg $ genDwarf  r
-      Just "elf"          -> rpg $ genElf    r
-      Just "hobbit"       -> rpg $ genHobbit r
-      Just "human"        -> rpg $ genHuman  r
+      Just "dwarf"        -> dwarf
+      Just "elf"          -> elf
+      Just "hobbit"       -> hobbit
+      Just "human"        -> human
       Just "king"         -> rpg $ king         $ genDwarf r
       Just "thane"        -> rpg $ thane        $ genDwarf r
       Just "dragonseeker" -> rpg $ dragonSeeker $ genDwarf r
@@ -34,4 +28,38 @@ module Main where
       Just "clanwarrior"  -> rpg $ clanWarrior  $ genDwarf r
       Just "hit"          -> tohit
       Just "wound"        -> towound
-      _ -> do { tohit; towound; rpg $ genDwarf r }
+      Just "party"        -> party
+      _ -> do { party; tohit; towound }
+
+  party :: IO ()
+  party = do
+    putStrLn "A Party of Four Adventurers..."
+    putStrLn "==============================\n"
+    dwarf
+    elf
+    hobbit
+    human
+
+  dwarf :: IO()
+  dwarf = do
+    r <- roll rollDice
+    rpg $ genDwarf r
+
+  elf :: IO()
+  elf = do
+    r <- roll rollDice
+    rpg $ genElf r
+
+  hobbit :: IO()
+  hobbit = do
+    r <- roll rollDice
+    rpg $ genHobbit r
+
+  human :: IO()
+  human = do
+    r <- roll rollDice
+    rpg $ genHuman r
+
+  -- | dice pool
+  rollDice :: DiceSet
+  rollDice = zModRoll["100", "d10"]
